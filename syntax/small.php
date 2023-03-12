d909d19a7cad4dcbd3df5ef695a1d2c79826d17c
Sascha Leib <saschaleib@users.noreply.github.com>
2023-03-12 13:57:44 +0100

    Small fix for `small`
    Pun intended.

--- syntax/small.php
+++ syntax/small.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Variable element component for the adhoctags plugin
+ * Side comment (small) element component for the adhoctags plugin
  *
- * Defines  <var> ... </var> syntax
- * More info: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/var
+ * Defines  <small> ... </small> syntax
+ * More info: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/small
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Anika Henke <anika@selfthinker.org>
  * @author     Sascha Leib <sascha.leib(at)kolmio.com>
  */
 
-class syntax_plugin_adhoctags_var extends syntax_plugin_adhoctags_abstractinline {
+class syntax_plugin_adhoctags_small extends syntax_plugin_adhoctags_abstractinline {
 
     protected $special_pattern = ''; // (no empty tags!)
-    protected $entry_pattern   = '<var\b.*?>(?=.*?</var>)';
-    protected $exit_pattern    = '</var>';
-	protected $output_tag      = 'var';
+    protected $entry_pattern   = '<small\b.*?>(?=.*?</small>)';
+    protected $exit_pattern    = '</small>';
+	protected $output_tag      = 'small';
 }
